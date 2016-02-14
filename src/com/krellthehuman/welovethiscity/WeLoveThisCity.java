4c7c3ba026b788497d2674d26b714e7037b8feba
KrellTheHuman <KrellTheHuman@gmail.com>
2016-02-13 16:45:13 -0800

    two views wired up with other placeholder views created

--- src/com/krellthehuman/welovethiscity/WeLoveThisCity.java
+++ src/com/krellthehuman/welovethiscity/WeLoveThisCity.java
@@ -2,14 +2,35 @@ package com.krellthehuman.welovethiscity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class WeLoveThisCity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+public class WeLoveThisCity extends Activity
+{
+    private ImageView _loadingTitle;
+    private View.OnClickListener _droidTapListener;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.title);
+
+        initializeApp();
+    }
+
+    private void initializeApp() {
+        _loadingTitle = (ImageView) findViewById(R.id.titleImageView);
+
+        _droidTapListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                TapDroid();
+            }
+        };
+        _loadingTitle.setOnClickListener(_droidTapListener);
+    }
+
+    private void TapDroid() {
         setContentView(R.layout.main);
+        System.out.println("title logo tapped. going to main.");
     }
 }
